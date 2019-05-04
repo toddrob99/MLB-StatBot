57a6ab28ad7962593dbceb5437a66b24a209ba54
todd <toddrob@gmail.com>
2019-05-04 15:37:39 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # MLB-StatBot
+
 Python script that replies to comments in configured subreddits with MLB data.
+
+This project was developed in Python 3.7, but should also work with Python 2.7.
+
+## Setup
+
+Install praw and MLB-StatsAPI:
+
+`pip install praw`
+`pip install MLB-StatsAPI`
+
+Run statbot\main.py in Python and either pass the subreddit(s) as a parameter or enter when prompted.
+
+`py -3 statbot\main.py MySubreddit`
+
+If you want to monitor multiple subreddits, separate them with +.
+
+`py -3 statbot\main.py MySubreddit+MyOtherSub`
+
+The first time you run the bot, you will be asked if you already have Reddit authentication information. 
+If so, enter Y and then paste the info as requested. If not, enter N and follow the prompts.
+
+Your Reddit authentication info will be stored in statbot\auth.py, and you can edit it there if needed.
+
+## Use
+
+Invoke the bot in a monitored subreddit by including the bot's reddit username in a comment. 
+
+Include a command, subject, and qualifier as needed, and the bot will reply with the requested data.
+
+## Commands
+
+### help
+Subject: None
+Qualifier: None
+Reply: Information about using the bot, including a list of commands.
+Example: `botname help`
+
+### careerstats
+Subject: Player name, enclosed in {} anywhere in your comment. Can be part of first name, last name, or full name in format 'Last, First'. Include enough for the bot to uniquely identify a single player, or you might not get the result you expect. For example, Aaron Nola's last name is part of Nolan Arenado's first name, so in order to get stats for Aaron Nola, try {nola,} (enough of 'last, first' format to be unique).
+Qualifier: Type of stats requested, anywhere in your comment. Available values: hitting/batting, pitching, and fielding. Include multiple types if you wish, or leave out the qualifier to return all available.
+Reply: The given player's career stats of the type(s) requested. If the player fielded in multiple positions, there will be separate sections in the reply for each position.
+Example: `botname careerstats {nola,} pitching`
+
+### seasonstats
+Subject: Player name, enclosed in {} anywhere in your comment. Can be part of first name, last name, or full name in format 'Last, First'. Include enough for the bot to uniquely identify a single player, or you might not get the result you expect. For example, Aaron Nola's last name is part of Nolan Arenado's first name, so in order to get stats for Aaron Nola, try {nola,} (enough of 'last, first' format to be unique).
+Qualifier: Type of stats requested, anywhere in your comment. Available values: hitting/batting, pitching, and fielding. Include multiple types if you wish, or leave out the qualifier to return all available.
+Reply: The given player's career stats of the type(s) requested. If the player fielded in multiple positions, there will be separate sections in the reply for each position.
+Example: `botname seasonstats {hoskins} batting`
+
+### score
+Subject: Team or division, enclosed within {} anyhwere in your comment. Can be part of the team's name, location, or team code. Include enough characters to uniquely identify a team. For example, 'lad' will return PhiLADelphia and not LA Dodgers as you might expect. To return scores for any games for a given division, include the abbreviation (nle, alw, etc.).
+Qualifier: None
+Reply: A summary of requested games, including teams, scores if game is in progress or final, inning if game is in progress, and status.
+Example: `botname score {nle}` or `botname score {phillies}
+
+### standings
+Subject: Division or league, enclosed within {} anywhere in your comment. Leave out for all divisions in both leagues. Include abbreviation for the league/division you want, for example {nl} or {alw}.
+Qualifier: Include `wildcard` or `wc` in your comment if you want wildcard standings to be included in the reply. 
+Reply: Division standings for the requested division/league(s), including wildcard standings if qualifier is present.
+Example: `botname standings {nle}` or `botname standings {al} wildcard`
+
+### nextgame
+Subject: Team name, enclosed in {} anywhere in your comment. Can be part of the team's name, location, or team code. Include enough characters to uniquely identify a team. For example, 'lad' will return PhiLADelphia and not LA Dodgers as you might expect. To return scores for any games for a given division, include the abbreviation (nle, alw, etc.).
+Qualifier: None
+Reply: Summary information about the given team's next game, including team names, scores if game is in progress or final, inning if game is in progress, and status.
+Example: `botname nextgame {oakland}`
+Note: The MLB API seems to get confused and return an unexpected game when requesting the next or previous game. This is how the API is returning the data, and there is nothing I can easily do to make this work better.
+
+### lastgame
+Subject: Team name, enclosed in {} anywhere in your comment. Can be part of the team's name, location, or team code. Include enough characters to uniquely identify a team. For example, 'lad' will return PhiLADelphia and not LA Dodgers as you might expect. To return scores for any games for a given division, include the abbreviation (nle, alw, etc.).
+Qualifier: None
+Reply: Summary information about the given team's last game, including team names, scores if game is in progress or final, inning if game is in progress, and status.
+Example: `botname lastgame {cubs}`
+Note: The MLB API seems to get confused and return an unexpected game when requesting the next or previous game. This is how the API is returning the data, and there is nothing I can easily do to make this work better.
+
+### winprob
+Subject: Team name, enclosed in {} anywhere in your comment. Can be part of the team's name, location, or team code. Include enough characters to uniquely identify a team. For example, 'lad' will return PhiLADelphia and not LA Dodgers as you might expect. To return scores for any games for a given division, include the abbreviation (nle, alw, etc.).
+Qualifier: None
+Reply: Game summary information, as well as current win probabilities for both teams. Works best while the team has a game in progress.
+Example: `botname winprob {phi}`
+
+## Copyright Notice
+
+This project uses the MLB-StatsAPI and PRAW packages to interface with the MLB and Reddit APIs. Use of MLB data is subject to the notice posted at http://gdx.mlb.com/components/copyright.txt.
---
+++ statbot/main.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# encoding=utf-8
+"""
+# MLB Stat Bot
+
+Created by Todd Roberts
+
+https://github.com/toddrob99/MLB-StatBot
+"""
+import sys
+if sys.version_info.major < 3:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+# Trying to support Python 2.7
+
+import version
+__version__ = version.VERSION
+"""Installed version of MLB-StatBot"""
+
+from datetime import datetime,timedelta
+import time
+import statsapi
+import praw
+
+class StatBot:
+    def __init__(self,sub):
+        self.sub = sub
+        self.replyFooter = '\n\n^^I ^^am ^^a ^^bot. ^^[[source](https://github.com/toddrob99/MLB-StatBot)] ^^[[feedback](https://reddit.com/message/compose?to=toddrob&subject=mlb-bot&message=)]'
+        print('StatBot starting up...')
+        reddit_clientId = None
+        reddit_clientSecret = None
+        reddit_refreshToken = None
+        
+        while not reddit_clientId and not reddit_clientSecret and not reddit_refreshToken:
+            try:
+                import auth
+                reddit_clientId = auth.reddit_clientId
+                reddit_clientSecret = auth.reddit_clientSecret
+                reddit_refreshToken = auth.reddit_refreshToken
+            except Exception as e:
+                print('Error importing authentication info from auth.py: {}'.format(e))
+            if reddit_clientId and reddit_clientSecret and reddit_refreshToken: continue
+            print('Reddit API authentication data not found. Let\'s fix that.')
+            if input('Do you already have a client id, client secret, and refresh token? (Y/N) ').lower() == 'y':
+                reddit_clientId = input('Enter client id: ')
+                reddit_clientSecret = input('Enter client secret: ')
+                reddit_refreshToken = input('Enter refresh token: ')
+            else:
+                print('Log in to Reddit as your bot user in your default browser, go to https://www.reddit.com/prefs/apps, click the "are you a developer? create an app" button, ')
+                print('and complete the form using http://localhost:8080 as the redirect uri. These fields can be changed later if needed.')
+                reddit_clientId = input('Enter client id (listed under your app name): ')
+                reddit_clientSecret = input('Enter client secret: ')
+                print('Your web browser will now open and take you to a Reddit authorization page. Ensure you are logged in as your bot user and click allow.')
+                r = praw.Reddit(client_id=reddit_clientId,
+                                client_secret=reddit_clientSecret,
+                                redirect_uri='http://localhost:8080',
+                                user_agent='MLB-StatBot by Todd Roberts'+__version__)
+                import webbrowser
+                url = r.auth.url(['identity', 'submit', 'edit', 'read'], '...', 'permanent')
+                print('Here is the URL in case you need to copy/paste into your browser: {}'.format(url))
+                webbrowser.open(url)
+                reddit_authCode = input('Enter the code from the address bar of your browser, everything after code=')
+                reddit_refreshToken = r.auth.authorize(reddit_authCode)
+                import os
+                try:
+                    f = open(os.path.dirname(os.path.realpath(__file__)) + '/auth.py', 'w+')
+                    f.write("#!/usr/bin/env python\nreddit_clientId='{}'\nreddit_clientSecret='{}'\nreddit_refreshToken='{}'\n".format(reddit_clientId,reddit_clientSecret,reddit_refreshToken))
+                    f.close()
+                except Exception as e:
+                    print('Error writing authentication info to auth.py: {}'.format(e))
+            import os
+            try:
+                f = open(os.path.dirname(os.path.realpath(__file__)) + '/auth.py', 'w+')
+                f.write("#!/usr/bin/env python\nreddit_clientId='{}'\nreddit_clientSecret='{}'\nreddit_refreshToken='{}'\n".format(reddit_clientId,reddit_clientSecret,reddit_refreshToken))
+                f.close()
+            except Exception as e:
+                print('Error writing authentication info to auth.py: {}'.format(e))
+
+        print('Initializing Reddit API with user agent MLB-StatBot by Todd Roberts {}'.format(__version__))
+        try:
+            self.r = praw.Reddit(client_id=reddit_clientId,
+                                 client_secret=reddit_clientSecret,
+                                 refresh_token=reddit_refreshToken,
+                                 user_agent='MLB-StatBot by Todd Roberts'+__version__)
+        except Exception as e:
+            print('Error authenticating with Reddit. Please check the values in auth.py and try again. Error message: {}'.format(e))
+        try:
+            if 'identity' in self.r.auth.scopes():
+                print('Authorized Reddit user: {}'.format(self.r.user.me()))
+        except Exception as e:
+            print('Reddit authentication failure. Please check the values in auth.py and try again. Error message: {}'.format(e))
+
+    def run(self):
+        while True:
+            print('Monitoring comments in the following subreddit(s): {}...'.format(self.sub))
+            for comment in self.r.subreddit(self.sub).stream.comments(skip_existing=True):
+                if str(self.r.user.me()).lower() in comment.body.lower() and comment.author != self.r.user.me():
+                    print('({}) {} - {}: {}\n'.format(comment.subreddit, comment.id, comment.author, comment.body))
+                    if 'help' in comment.body.lower():
+                        replyText = 'Invoke me by including my name anywhere in your comment.\n\n'
+                        replyText += 'Include an available command in your comment: [help, score, careerstats, seasonstats, standings, winprob], '
+                        replyText += 'along with the subject in curly brackets.\n\n'
+                        replyText += 'For stats commands, you can also include the type: [hitting, pitching, fielding].\n\n'
+                        replyText += 'For example, `careerstats {hamels} pitching` or `score {phillies}` or `standings {nle}` '
+                        replyText += '(try including the word wildcard when asking for standings).\n\n'
+                        replyText += 'I am currently monitoring the following subreddit(s): ' + self.sub + '.'
+                        comment.reply(replyText + self.replyFooter)
+                    elif 'seasonstats' in comment.body.lower():
+                        try:
+                            who = statsapi.lookup_player(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                            what = []
+                            if 'hitting' in comment.body.lower() or 'batting' in comment.body.lower(): what.append('hitting')
+                            if 'pitching' in comment.body.lower(): what.append('pitching')
+                            if 'fielding' in comment.body.lower(): what.append('fielding') 
+                            if len(what):
+                                stats = statsapi.player_stats(who,str(what).replace('\'','').replace(' ',''),'season')
+                            else:
+                                stats = statsapi.player_stats(who,type='season')
+                            replyText = '\n    ' + stats.replace('\n','\n    ')
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'careerstats' in comment.body.lower():
+                        try:
+                            who = statsapi.lookup_player(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                            what = []
+                            if 'hitting' in comment.body.lower() or 'batting' in comment.body.lower(): what.append('hitting')
+                            if 'pitching' in comment.body.lower(): what.append('pitching')
+                            if 'fielding' in comment.body.lower(): what.append('fielding') 
+                            if len(what):
+                                stats = statsapi.player_stats(who,str(what).replace('\'','').replace(' ',''),'career')
+                            else:
+                                stats = statsapi.player_stats(who,type='career')
+                            replyText = '\n    ' + stats.replace('\n','\n    ')
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'nextgame' in comment.body.lower():
+                        try:
+                            who = statsapi.lookup_team(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                            next = statsapi.next_game(who)
+                            game = statsapi.schedule(game_id=next)
+                            replyText = game[0]['summary']
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'lastgame' in comment.body.lower():
+                        try:
+                            who = statsapi.lookup_team(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                            last = statsapi.last_game(who)
+                            game = statsapi.schedule(game_id=last)
+                            replyText = game[0]['summary']
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'score' in comment.body.lower():
+                        try:
+                            who = comment.body[comment.body.find('{')+1:comment.body.find('}')].lower()
+                            if who in ['nle','nlw','nlc','ale','alw','alc','all']:
+                                todaysGames = statsapi.get('schedule',{'fields':'dates,date,games,gamePk,teams,away,team,division,abbreviation','sportId':1,'hydrate':'team(division)'})
+                                gamePks = ''
+                                for i in (x['gamePk'] for x in todaysGames['dates'][0]['games'] if who == 'all' or x['teams']['away']['team']['division']['abbreviation'].lower() == who or x['teams']['home']['team']['division']['abbreviation'].lower() == who):
+                                    gamePks += '{},'.format(i)
+                                if len(gamePks):
+                                    if gamePks[-1] == ',': gamePks = gamePks[:-1]
+                                games = statsapi.schedule(date=datetime.today().strftime('%m/%d/%Y'),game_id=gamePks)
+                                replyText = ''
+                                for game in games:
+                                    replyText += '\n    ' + game['summary']
+                            else:
+                                who = statsapi.lookup_team(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                                game = statsapi.schedule(date=datetime.today().strftime('%m/%d/%Y'),team=who)
+                                replyText = game[0]['summary']
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'standings' in comment.body.lower():
+                        try:
+                            if comment.body.find('{') != -1:
+                                who = comment.body[comment.body.find('{')+1:comment.body.find('}')].lower()
+                            else: who='all'
+                            wc = True if any(True for x in ['wc','wildcard','wild card'] if x in comment.body) else False
+                            if who == 'all':
+                                standings = statsapi.standings(date=datetime.today().strftime('%m/%d/%Y'),include_wildcard=wc)
+                            elif who in ['nle','nlw','nlc','ale','alw','alc']:
+                                standings = statsapi.standings(date=datetime.today().strftime('%m/%d/%Y'),division=who,include_wildcard=wc)
+                            elif who in ['nl','al']:
+                                leagueId = 103 if who=='al' else 104
+                                standings = statsapi.standings(leagueId=leagueId,date=datetime.today().strftime('%m/%d/%Y'),include_wildcard=wc)
+                            replyText = '\n    {}'.format(standings.replace('\n','\n    '))
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+                    elif 'winprob' in comment.body.lower():
+                        try:
+                            who = statsapi.lookup_team(comment.body[comment.body.find('{')+1:comment.body.find('}')])[0]['id']
+                            game = statsapi.schedule(date=datetime.today().strftime('%m/%d/%Y'),team=who)[0]
+                            contextMetrics = statsapi.get('game_contextMetrics',{'gamePk':game['game_id']})
+                            away_win_prob = contextMetrics.get('awayWinProbability','-')
+                            home_win_prob = contextMetrics.get('homeWinProbability','-')
+                            replyText = '\n    ' + game['summary'] + '\n'
+                            replyText += '    Current win probabilities: ' + game['away_name'] + ' ' + str(away_win_prob) + '%, ' + game['home_name'] + ' ' + str(home_win_prob) + '%'
+                            comment.reply(replyText + self.replyFooter)
+                        except Exception as e:
+                            print('Error replying to comment: {}'.format(e))
+
+        return
+
+if __name__=='__main__':
+    if len(sys.argv)>1:
+        sub = sys.argv[1]
+    else:
+        sub= input('What subreddit(s) should I monitor? (Include multiple in the format sub1+sub2+sub3): ')
+    mlbbot = StatBot(sub)
+    mlbbot.run()
